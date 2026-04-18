27983136c10ddf5d178d35761d45718dce4dfdef
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-04-18 12:36:53 +0530

    PATTERNS DONE

--- complete_java/Javabasics.java
+++ complete_java/Javabasics.java
@@ -252,21 +252,38 @@
 
 
 //--display all numbers entered by users except multiple of 10;
-import java.util.*;
+// import java.util.*;
+// public class Javabasics{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         int nuumber = 0;
+//         do{
+//             System.out.println("Enter a number");
+//             int number = sc.nextInt();
+
+//             if(number % 10 == 0){
+//                 continue;
+//             }
+
+//             System.out.println(number);
+//         }while(true);
+//     }
+// }
+
+
+
+
 public class Javabasics{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        char ch = 'A';
 
-        int nuumber = 0;
-        do{
-            System.out.println("Enter a number");
-            int number = sc.nextInt();
-
-            if(number % 10 == 0){
-                continue;
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
             }
-
-            System.out.println(number);
-        }while(true);
+            System.out.println();
+        }
     }
 }
