cb823dffd06c90174cbee18f2ab999ebf94be544
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-06-20 08:21:10 +0530

    Linked List IsPalindrome ??

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,12 +1,14 @@
 //---------------LINKED LIST----------------
 
 //create one class as a whole linked list
-public class LinkedList{
-    class Node{
+public class LinkedList {
+
+    class Node {
+
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
@@ -17,12 +19,12 @@ public class LinkedList{
     public static int size;
 
     //method to ADDFIRST new NODE (elements) in LL
-    public void addFirst(int data){
+    public void addFirst(int data) {
         //step-1: create new node
         Node newNode = new Node(data);
         size++;
         //if Linked List is Empty
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -35,12 +37,12 @@ public class LinkedList{
     }
 
     //ADDLAST new NODE (elements) in LL
-    public void addLast(int data){
+    public void addLast(int data) {
         //step-1: create newNode
         Node newNode = new Node(data);
         size++;
         //if Linked List is null
-        if(head == null){
+        if (head == null) {
             tail.next = newNode;
             head = tail = newNode;
             return;
@@ -54,22 +56,22 @@ public class LinkedList{
     }
 
     //ADD IN THE MIDDLE
-    public void add(int index, int data){
+    public void add(int index, int data) {
         Node newNode = new Node(data);
         //if index == 0.. means inserting at first means head should be handled properly.. thus
-        if(index == 0){
+        if (index == 0) {
             addFirst(data);
             return;
         }
         size++;
         //if linkedlist is empty ...
-        if(index >0 && head == null){
-            System.out.println("Linked list is empty and you can't insert at "+index);
+        if (index > 0 && head == null) {
+            System.out.println("Linked list is empty and you can't insert at " + index);
             return;
         }
         Node temp = head;
         int i = 0;
-        while(i != index - 1){
+        while (i != index - 1) {
             temp = temp.next;
             i++;
         }
@@ -79,12 +81,12 @@ public class LinkedList{
     }
 
     //REMOVE FIRST
-    public int removeFirst(){
+    public int removeFirst() {
         //size is 0
-        if(size == 0){
+        if (size == 0) {
             System.out.println("LL is Empty");
             return Integer.MAX_VALUE;
-        }else if(size == 1){
+        } else if (size == 1) {
             int val = head.data;
             head = tail = null;
             size = 0;
@@ -98,11 +100,11 @@ public class LinkedList{
     }
 
     //REMOVE LAST
-    public int removeLast(){
-        if(size == 0){
+    public int removeLast() {
+        if (size == 0) {
             System.out.println("LL is empty.");
             return Integer.MIN_VALUE;
-        }else if(size == 1){
+        } else if (size == 1) {
             int val = head.data;
             head = tail = null;
             size = 0;
@@ -110,7 +112,7 @@ public class LinkedList{
         }
 
         Node prev = head;
-        for(int i = 1; i < size - 1; i++){
+        for (int i = 1; i < size - 1; i++) {
             prev = prev.next;
         }
         int val = tail.data; //prev.next.data;
@@ -122,15 +124,15 @@ public class LinkedList{
 
     //Method to PRINT linkedlist
     //we already have head thus no need to pass parameter
-    public void print(){
+    public void print() {
         //if linkedlist is Empty
-        if(head == null){
+        if (head == null) {
             System.out.println("Linked List is Empty.");
             return;
         }
 
         Node temp = head;
-        while(temp != null){
+        while (temp != null) {
             System.out.print(temp.data + "-->");
             temp = temp.next;
         }
@@ -138,15 +140,15 @@ public class LinkedList{
     }
 
     //ITERATIVE SEARCH
-    public int inrSearch(int key){
+    public int inrSearch(int key) {
         Node temp = head;
         int idx = 0;
 
-        if(temp.data == key){
+        if (temp.data == key) {
             return idx;
         }
-        while(temp != null){
-            if(temp.data == key){
+        while (temp != null) {
+            if (temp.data == key) {
                 return idx;
             }
             temp = temp.next;
@@ -157,31 +159,32 @@ public class LinkedList{
     }
 
     //ITERATIVE SEARCH
-    public int helper(Node head, int key){
-        if(head == null){
+    public int helper(Node head, int key) {
+        if (head == null) {
             return -1;
         }
-        if(head.data == key){
+        if (head.data == key) {
             return 0;
         }
 
         int idx = helper(head.next, key);
-        if(idx == -1){
+        if (idx == -1) {
             return -1;
         }
         return idx + 1;
     }
-    public int recSearch(int key){
+
+    public int recSearch(int key) {
         return helper(head, key);
     }
 
     //REVERSE ITERATIVE
-    public void reverse(){
+    public void reverse() {
         Node prev = null;
         Node curr = tail = head;  //Node curr = head
         Node next;
 
-        while(curr != null){
+        while (curr != null) {
             next = curr.next;
             curr.next = prev;
             prev = curr;
@@ -191,49 +194,94 @@ public class LinkedList{
         head = prev;
     }
 
-    
+    //REMOVE NTH NODE FROM END
+    public void removeNth(int n) {
 
-    public static void main(String[] args){
+        if (size == 0) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+
+        if (n > size) {
+            System.out.println("Invalid Position");
+            return;
+        }
+
+        // remove head
+        if (n == size) {
+            head = head.next;
+            size--;
+            return;
+        }
+
+        int newN = size - n;
+
+        Node prev = head;
+
+        for (int i = 1; i < newN; i++) {
+            prev = prev.next;
+        }
+
+        prev.next = prev.next.next;
+        size--;
+    }
+
+    //FIND MID USING SLOW-FAST APPROACH
+    public Node findMid(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        //continue only when both conditions are true else stop.
+        while(fast != null || fast.next != null){
+            slow = slow.next; //move by +1
+            fast = fast.next.next; //move by +2
+        }
+        return slow; //means slow is my mid
+    }
+
+    public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         //addFirst
-        ll.addFirst(1);
+        ll.addFirst(5);
         ll.print();
-        ll.addFirst(2);
+        ll.addFirst(4);
         ll.print();
         //addMiddle
-        ll.add(0,10); ll.print();
-        ll.add(0,9); ll.print();
-        ll.add(2,11); ll.print();
-        //addLast
-        ll.addLast(3);
+        ll.add(0, 2);
         ll.print();
-        ll.addLast(4);
+        ll.add(0, 1);
+        ll.print();
+        ll.add(2, 3);
+        ll.print();
+        //addLast
+        ll.addLast(6);
+        ll.print();
+        ll.addLast(7);
         ll.print();
         //9->10->11->2->1->3->4
         //size = 7
         System.out.println("Size of LL: " + ll.size);
 
         //REMOVE FIRST
-        ll.removeFirst();
-        ll.print();
-        System.out.println("Size of LL: " + ll.size);
-
+        // ll.removeFirst();
+        // ll.print();
+        // System.out.println("Size of LL: " + ll.size);
         //REMOVE LAST
-        ll.removeLast();
-        ll.print();
-        System.out.println("Size of LL: " + ll.size);
-
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println("Size of LL: " + ll.size);
         //ITERATIVE SEARCH
         // System.out.println(ll.inrSearch(2));
         // System.out.println(ll.inrSearch(50));
-
         //RECURSIVE SEARCH
-        System.out.println(ll.recSearch(2));
-        System.out.println(ll.recSearch(50));
-
+        // System.out.println(ll.recSearch(2));
+        // System.out.println(ll.recSearch(50));
         //REVERSE
-        ll.print(); 
-        ll.reverse();
+        // ll.print(); 
+        // ll.reverse();
+        // ll.print();
+        //REMOVE NTH FROM END
+        ll.removeNth(3);  //here initially LL : 
         ll.print();
     }
 }
