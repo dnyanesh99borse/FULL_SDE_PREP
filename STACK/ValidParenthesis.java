6082173af6ea6172a4237e90d76e8b2028aaec02
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-06-26 06:59:30 +0530

    Valid Parenthesis

---
+++ STACK/ValidParenthesis.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class ValidParenthesis{
+    public static boolean validParenthesis(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            //opening
+            if(ch == '('|| ch == '{' || ch == '['){
+                s.push(ch);
+            }else{
+                //means all closing bracket string or closing intially
+                if(s.isEmpty()){
+                    return false;
+                }
+                //now check valid pairs
+                char top = s.peek();
+                if((top == '{' && ch == '}') 
+                || (top == '(' && ch == ')') 
+                || (top == '[' && ch == ']')){
+                    s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        if(s.isEmpty()){  //means there still some braces means invalid
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args){
+        // String str = "({}]})";
+        // String str = "({[]})";
+        String str = ")(";
+
+        System.out.println(validParenthesis(str));
+    }
+}
