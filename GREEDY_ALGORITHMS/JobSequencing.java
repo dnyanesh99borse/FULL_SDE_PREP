3e35c717253b4d6754c37370ca1715d22444a1f5
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-06-29 08:11:45 +0530

    JOB SEQUENCING USING GREEDY

---
+++ GREEDY_ALGORITHMS/JobSequencing.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class JobSequencing{
+    static class job{
+        int deadline;
+        int profit;
+        int id;
+
+        public job( int id, int d, int p){
+            this.id = id;
+            this.deadline = d;
+            this.profit = p;
+        }
+    }
+    public static void main(String[] args){
+        int[][] jobsInfo = {{4,20},{1,10},{1,40},{1,30}};
+
+        ArrayList<job> jobs = new ArrayList<>();
+
+        for(int i=0; i<jobsInfo.length; i++){
+            jobs.add(new job(i, jobsInfo[i][0], jobsInfo[i][1]));
+        }
+        //sorted in descending order of profit
+        Collections.sort(jobs, (obj1,obj2) -> obj2.profit - obj1.profit);
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for(int i=0; i<jobs.size(); i++){
+            job curr = jobs.get(i);
+            if(curr.deadline > time){
+                seq.add(curr.id);
+                time++;
+            }
+        }
+
+        //print seq
+        System.out.println("max jobs = "+seq.size());
+        for(int i=0; i<seq.size(); i++){
+            System.out.println((char)('A' + i) + " -> " + seq.get(i));
+        }
+        System.out.println();
+    }
+}
