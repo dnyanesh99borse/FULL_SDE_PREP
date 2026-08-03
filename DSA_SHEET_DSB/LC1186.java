1ea8371eefc257946e718dbd4020dbcb154d23f7
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-08-04 01:54:34 +0530

    DUTCH NATIONAL FLAG

---
+++ DSA_SHEET_DSB/LC1186.java
@@ -0,0 +1,11 @@
+//-------------------DUTCH NATIONAL FLAG PATTERN-------------------------------
+
+//public class LC1186 {
+//    public static int maximumSum(int[] arr) {
+//
+//    }
+//    public static void main(String[] args){
+//        int[] arr = {1,-2,0,3};
+//        System.out.println(maximumSum(arr));
+//    }
+//}
---
+++ DSA_SHEET_DSB/LC75SortColors.java
@@ -0,0 +1,36 @@
+public class LC75SortColors {
+    public static void sortColors(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] nums = {2,0,2,1,1,0};
+        sortColors(nums);
+
+        for(int num: nums){
+            System.out.print(num+" ");
+        }
+    }
+}
